0fcf9f9a71857b794b7414e0023421b5fa72eaf6
OksanaKozak <oksanakozak2016@gmail.com>
2023-03-17 18:18:02 +0200

    add 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,34 @@
 const liItemEl = document.querySelectorAll('.item');
-console console.log(liItemEl);
+console.log('Number of categories:', liItemEl.length);
+
+const ItemEl = document.querySelector('.item');
+
+const title = ItemEl.firstElementChild;
+console.log(title.textContent);
+
+
+
+
+// categories.forEach((el) => {
+//     console.log(`Number of categories: ${el.firstElementChild.textContent}`);
+
+
+
+
+
+// liItemEl.forEach(function (number, index) {
+//     console.log(`Индекс ${index}, значение ${number}`);
+//   });
+
+//   const numbers = [5, 10, 15, 20, 25];
+
+
+// // Перебирающий forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+
+
+
 
