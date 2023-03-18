02fa89d35bc8c28ea22e0284fd57e286f7c89d3d
OksanaKozak <oksanakozak2016@gmail.com>
2023-03-18 22:15:29 +0200

    add task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const fragment = document.createDocumentFragment();
+ingredients.forEach(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.className = 'item';
+  fragment.appendChild(liEl);
+});
+document.body.appendChild(fragment);
+
+
