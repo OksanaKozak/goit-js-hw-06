96d2df1d0eebe0194dfd308ecefc462871f4d85a
OksanaKozak <oksanakozak2016@gmail.com>
2023-03-20 22:10:10 +0200

    add task5

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,16 @@
-lrt counterValue = 0;
+let counterValue = 0;
+
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+const span = document.querySelector('#value');
+
+
+buttonIncrement.addEventListener('click', () => {
+  counterValue +=1;
+  span.textContent = counterValue;
+  });
+
+buttonDecrement.addEventListener('click', () => {
+    counterValue -=1;
+    span.textContent = counterValue;
+    });
