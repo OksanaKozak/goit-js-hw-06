09c9b6be56212aca3d717f77045ed8e555ffda1f
OksanaKozak <oksanakozak2016@gmail.com>
2023-03-22 21:58:58 +0200

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+
+  const { email, password } = loginForm.elements;
+
+  if (email.value === '' || password.value === '') {
+    alert('Всі поля повинні бути заповнені');
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  }
+});
 
